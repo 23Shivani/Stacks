1378617005a4cb4eddb88aab0c9df19ea52ff872
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-31 00:38:12 +0530

    Added: Create Stack Program

---
+++ src/com/bridgelabz/MyStack.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class MyStack {
+	MyStackNode head;
+	public void push (int data) {
+		MyStackNode newNode = new MyStackNode();
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+		
+	}
+	public void printStack() {
+		MyStackNode tempNode = head;
+		while(tempNode != null) {
+			System.out.println(tempNode.data);
+			tempNode =tempNode.next;
+		}
+	}
+}
---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		MyStack newStack = new MyStack();
+		newStack.push(30);
+		newStack.push(50);
+		newStack.push(70);
+		newStack.printStack();
+}
+}
